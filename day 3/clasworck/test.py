d16e866a1d6a1ac9f8011a735c6827e74bb1b8fc
goga kvaraia <165575023+gogaChoqela@users.noreply.github.com>
2024-06-03 19:06:38 +0400

    Add files via upload

---
+++ day 3/clasworck/test.py
@@ -0,0 +1,42 @@
+print("hello world \n am goga")
+
+print('''goga
+goga
+ goga''')
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+
+print("goga var me kvaraia")
+
+
+#________________________________________________________________________
+# integer
+
+print (100)
+print (927)
+print (-3664)
+print (300 +232)
+print(int(20.7))
+
+print(5 *5)
+print(4/2)
+
+#%- იყოფა და გამოაქვს ნაშთი
+
+print (5 % 2)
+
+#______________________________________________
+
+#varable bame = varible value
+toy="toy car"
+clothes= "t-shirt"
+print (toy)
---
+++ day 3/homeworckk/homeworck.py
@@ -0,0 +1,22 @@
+football_1="ლევან კობიაშვილი "
+football_2="შოთა იამანიძე "
+football_3="ოთარ გაბელია "
+football_4="შოთა იამანიძე "
+football_5="ვიტარი დარასელია "
+#_____________________________________
+basketball_1="გოგა ბითაძე "
+basketball_2="გიორგი ტურძილაძე "
+basketball_3="მიხეილ ბერიშვილი "
+basketball_4="დუდა სანაძე "
+basketball_5="სანდრო მამუკელაშვილი "
+#____________________________________
+sport_1= "football "
+sport_2= "basketball "
+#_________________________________________
+
+
+print (sport_1 + ":"+ football_1 + football_2 + football_3 + football_4 + football_5)
+print (sport_2 + ":" + basketball_1 + basketball_2 + basketball_3 + basketball_4 + basketball_5)
+
+
+
